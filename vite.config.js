657ef96d62a4f965fbc00c07a1f03c5e0e675e78
Becario <becario@KAPLCS68.local>
2025-05-19 16:06:59 -0600

    Agregamos GithubPages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/mrbugs16/github-profile'       
+  base: '/github-profile/'   // ← sin los <> y con / al inicio y al final
 });
