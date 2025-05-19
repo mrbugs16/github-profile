6143f19bd202232de12782921b69419a9bbcda78
Becario <becario@KAPLCS68.local>
2025-05-19 16:07:56 -0600

    Actualizamos Github Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/github-profile/'   // ← sin los <> y con / al inicio y al final
+  base: '/github-profile/'   
 });
